2b0efc541c1e90eba24345c042a301058efd8876
Jerry Jackson <jerry.jackson@acommitteeoflunatics.com>
2014-08-21 11:29:32 -0500

    fixing syntax error in recipes/debian.rb

--- recipes/debian.rb
+++ recipes/debian.rb
@@ -7,7 +7,7 @@
 include_recipe "apt"
 
 case case node[:platform_version]
-when < "7.0"
+when < 7.0
 	apt_repository "backports" do
 		uri "http://mirror.yandex.ru/debian-backports"
 		distribution "oldstable-backports"
